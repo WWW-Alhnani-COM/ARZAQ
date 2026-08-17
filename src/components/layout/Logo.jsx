1ddef6c3ea062922ba72ccb04b5fe1e33094f573
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2026-08-17 20:33:51 +0300

    Update Logo.jsx

--- src/components/layout/Logo.jsx
+++ src/components/layout/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo({ light }) {
       <img
         src={logo}
         alt="ARZAQ"
-        className="w-[50px] h-[50px] object-contain"
+        className="w-[60px] h-[60px] object-contain"
       />
     </div>
   );
